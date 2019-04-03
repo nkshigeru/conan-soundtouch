73885d00b9dcffffcb455aedca4d8949583f2bf9
nkshigeru <1712694+nkshigeru@users.noreply.github.com>
2019-04-03 19:00:57 +0900

    Works on mac

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,5 @@
 import os
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, tools, CMake, AutoToolsBuildEnvironment
 
 
 class SoundtouchConan(ConanFile):
@@ -11,27 +11,45 @@ class SoundtouchConan(ConanFile):
     description = "<Description of Soundtouch here>"
     topics = ("<Put some tag here>", "<here>", "<and here>")
     settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = "shared=True"
     exports = []
     exports_sources = ["CMakeLists.txt"]
     generators = "cmake"
+    source_subfolder = "soundtouch"
 
     def source(self):
         url = "https://gitlab.com/soundtouch/soundtouch/-/archive/{version}/soundtouch-{version}.tar.gz".format(version=self.version)
         tools.get(url)
-        os.rename("soundtouch-{version}".format(version=self.version), "soundtouch")
+        os.rename("soundtouch-{version}".format(version=self.version), self.source_subfolder)
         tools.replace_in_file("soundtouch/source/SoundTouchDLL/SoundTouchDLL.rc",
             "afxres.h", "winres.h")
 
     def build(self):
-        cmake = CMake(self)
-        cmake.configure()
-        cmake.build()
+        if self.settings.os == "Windows":
+            cmake = CMake(self)
+            cmake.configure()
+            cmake.build()
+        else:
+            with tools.chdir(self.source_subfolder):
+                self.run("sh bootstrap")
+                env_build = AutoToolsBuildEnvironment(self)
+                def option_value(b):
+                    return "yes" if b else "no"
+                args = [
+                    "--enable-shared=" + option_value(self.options.shared),
+                    "--enable-static=" + option_value(not self.options.shared),
+                ]
+                env_build.configure(args=args)
+                env_build.make()
+                env_build.install()
 
     def package(self):
-        #self.copy("soundtouch/include/SoundTouch.h", dst="include", keep_path=False)
-        self.copy("soundtouch/source/SoundTouchDLL/SoundTouchDLL.h", dst="include", keep_path=False)
-        self.copy("lib/SoundTouch.lib", dst="lib", keep_path=False)
-        self.copy("bin/SoundTouch.dll", dst="bin", keep_path=False)
+        if self.settings.os == "Windows":
+            #self.copy("soundtouch/include/SoundTouch.h", dst="include", keep_path=False)
+            self.copy("soundtouch/source/SoundTouchDLL/SoundTouchDLL.h", dst="include", keep_path=False)
+            self.copy("lib/SoundTouch.lib", dst="lib", keep_path=False)
+            self.copy("bin/SoundTouch.dll", dst="bin", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = ["SoundTouch"]
--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -1,5 +1,16 @@
 #include <stdio.h>
+#ifdef _WIN32
 #include "SoundTouchDLL.h"
+#else
+
+#include "soundtouch/SoundTouch.h"
+
+static inline const char *soundtouch_getVersionString()
+{
+	return soundtouch::SoundTouch::getVersionString();
+}
+
+#endif
 
 int main() {
     printf("SoundTouch %s\n", soundtouch_getVersionString());
